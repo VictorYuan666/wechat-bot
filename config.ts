87f95de4a314b91f458220625d93d3aa4ffb8a2d
VictorYuan666 <yuanxiaowei1991@gmail.com>
2021-10-10 03:49:40 +0000

    feat: add poem api

--- config.ts
+++ config.ts
@@ -2,5 +2,5 @@ export default {
   weatherKey: "921b76882a7045ae869d7673f5570317", // 和风天气key
   location: "101210113", // 和风天气-成都武侯区的id
   type: "1,3,9", // 和风天气-生活指数type
-  tianXingKey: "eb75297818f2413f24e1f1f76662bccd", // 天行数据的key
+  tianXingKey: "06888a8ba73ffb85d6a486033d7f7f17", // 天行数据的key
 };
--- index.ts
+++ index.ts
@@ -13,20 +13,47 @@ Wechaty.instance() // Global Instance
     console.log(qrImgUrl);
   })
   .on("login", (user) => console.log(`User ${user} logged in`))
-  .on("message", async(msg: Message) => {
+  .on("message", async (msg: Message) => {
     console.log(`Message: ${msg}`);
-    if (msg.text() == "#one") {
-      const url = `http://api.tianapi.com/txapi/one/index?key=${config.tianXingKey}`;
-      const res: any = await axios.get(url);
-      console.log(JSON.stringify(res));
-      msg.say(res.data.newslist[0].word);
+    switch (msg.text()) {
+      case "one":
+        handleOneAPI(msg)
+        break;
+      case "poem":
+        const url = `http://api.tianapi.com/txapi/verse/index?key=${config.tianXingKey}`
+        handlePoem(msg)
+
+
+      default:
+        break;
     }
+
   }).start();
+
+async function handleOneAPI(msg: Message) {
+  const url = `http://api.tianapi.com/txapi/one/index?key=${config.tianXingKey}`;
+  const res: any = await axios.get(url);
+
+  msg.say(res.data.newslist[0].word);
+
+}
+
+async function handlePoem(msg: Message) {
+  const url = `http://api.tianapi.com/txapi/verse/index?key=${config.tianXingKey}`;
+  const res: any = await axios.get(url);
+  const {content,source} = res.data.newslist[0]
+
+  msg.say(`${content}——${source}`);
+}
+
+
+
 async function main() {
   const weatherUrl = `https://devapi.qweather.com/v7/weather/3d?key=${config.weatherKey}&location=${config.location}`;
   const lifeUrl = `https://devapi.qweather.com/v7/indices/1d?key=${config.weatherKey}&location=${config.location}&type=${config.type}`;
 
-  const url = `http://api.tianapi.com/txapi/one/index?key=${config.tianXingKey}`;
+  // const url = `http://api.tianapi.com/txapi/one/index?key=${config.tianXingKey}`;
+  const url = `http://api.tianapi.com/txapi/verse/index?key=${config.tianXingKey}`;
   const { data } = await axios.get(url);
   console.log(JSON.stringify(data));
 }
