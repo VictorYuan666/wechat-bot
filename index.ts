671649e01e5809213e89c278bd97e3ce794f4fee
袁小威 <yuanxiaowei@u51.com>
2021-10-11 17:18:36 +0800

    feat: add schedule

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { Wechaty, Message, Contact } from "wechaty"; // import { Wechaty } from 'wechaty'
 import QRTerminal from "qrcode-terminal";
 import axios from "axios";
-import schedule from 'node-schedule'
+import schedule from "node-schedule";
 import config from "./config";
 
-const bot = new Wechaty()
+const bot = new Wechaty();
 
-bot.on("scan", (qrcode: string, status: number) => {
+bot
+  .on("scan", (qrcode: string, status: number) => {
     QRTerminal.generate(qrcode, { small: true });
 
     const qrImgUrl =
@@ -19,22 +20,22 @@ bot.on("scan", (qrcode: string, status: number) => {
     console.log(`Message: ${msg}`);
     switch (msg.text()) {
       case "one":
-        handleOneAPI(msg)
+        handleOneAPI(msg);
         break;
       case "poem":
-        handlePoem(msg)
+        handlePoem(msg);
         break;
       case "en":
-        handleEn(msg)
+        handleEn(msg);
         break;
       case "weather":
-        handleWeather(msg)
-          break;
+        handleWeather(msg);
+        break;
       case "test":
-        // const contact:any = await bot.Contact.find({name: '八月'}) 
+        // const contact:any = await bot.Contact.find({name: '八月'})
         // contact.say('hello')
-        const room:any = await bot.Room.find({topic: '801'})
-        await room.say('Hello world!')
+        const room: any = await bot.Room.find({ topic: "801" });
+        await room.say("Hello world!");
         break;
       default:
         break;
@@ -47,13 +48,12 @@ async function handleOneAPI(msg: Message) {
   const res: any = await axios.get(url);
 
   msg.say(res.data.newslist[0].word);
-
 }
 
 async function handlePoem(msg: Message) {
   const url = `http://api.tianapi.com/txapi/verse/index?key=${config.tianXingKey}`;
   const res: any = await axios.get(url);
-  const {content,source} = res.data.newslist[0]
+  const { content, source } = res.data.newslist[0];
 
   msg.say(`${content}——${source}`);
 }
@@ -61,11 +61,11 @@ async function handlePoem(msg: Message) {
 async function handleEn(msg: Message) {
   const url = `http://api.tianapi.com/txapi/ensentence/index?key=${config.tianXingKey}`;
   const res: any = await axios.get(url);
-  const {en,zh} = res.data.newslist[0]
+  const { en, zh } = res.data.newslist[0];
 
   msg.say(`${en}\n${zh}`);
 }
-async function handleWeather(msg: Message) {
+async function handleWeather(msg?: Message) {
   // {
   //   "area": "西湖",
   //   "date": "2021-10-10",
@@ -92,10 +92,14 @@ async function handleWeather(msg: Message) {
   // },
   const url = `http://api.tianapi.com/txapi/tianqi/index?key=${config.tianXingKey}&city=%E8%A5%BF%E6%B9%96%E5%8C%BA`;
   const res: any = await axios.get(url);
-  const {area,weather,highest,lowest,windsc,tips} = res.data.newslist[0]
-  console.log(res)
+  const { area, weather, highest, lowest, windsc, tips } = res.data.newslist[0];
+  console.log(res);
+  const text = `今日${area}区天气${weather} 最高气温:${highest} 最低气温:${lowest} 风力:${windsc}\n${tips}`;
+  if (msg) {
+    msg.say(text);
+  }
 
-  msg.say(`今日${area}区天气${weather} 最高气温:${highest} 最低气温:${lowest} 风力:${windsc}\n${tips}`);
+  return text;
 }
 
 async function main() {
@@ -106,20 +110,21 @@ async function main() {
   // const url = `http://api.tianapi.com/txapi/verse/index?key=${config.tianXingKey}`;
   // const { data } = await axios.get(url);
   // console.log(JSON.stringify(data));
-  const schedule = require('node-schedule');
 
-// 定义规则
-let rule = new schedule.RecurrenceRule();
-// rule.second = [0, 10, 20, 30, 40, 50]; // 每隔 10 秒执行一次
-rule.hour =8;
-rule.minute =18;
-rule.second =0;
+  // 定义规则
+  let rule = new schedule.RecurrenceRule();
+  // rule.second = [0, 10, 20, 30, 40, 50]; // 每隔 10 秒执行一次
+  rule.hour = 23;
+  rule.minute = 30;
+  rule.second = 0;
 
-// 启动任务
-let job = schedule.scheduleJob(rule, () => {
-  console.log(`${'-------'}%0a${new Date()}`);
-});
-
+  // 启动任务
+  let job = schedule.scheduleJob(rule, async () => {
+    console.log(`${"-------"}\n${new Date()}`);
+    const room: any = await bot.Room.find({ topic: "801" });
+    const weatherText = await handleWeather();
+    await room.say(weatherText);
+  });
 }
 
-// main()
+main();
